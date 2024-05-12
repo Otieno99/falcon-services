91ded5294b76cdf429ca1e73f0cd1479c13a75ef
website <otienoi248@gmail.com>
2024-05-13 00:34:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,182 @@
+<?php
+  require_once "header.html";
+?>
+
+<div>
+    <h2>Welcome to our Website</h2>
+</div>
+<div>
+    <h3 class="abt">About</h3>
+    <p class="abt">
+    Falcon Computec is a Kenyan service provider company, 
+    that deals with branding of; vehical decals, sticker, 
+    envelop, letterheads, school and work id's, calenders, 
+    and t-shirt printing. <br>
+    Graphic designs of: posters, flyers, logos, catalogues, 
+    invitation cards, banners, brochures, award certificates, event programs e.t.c Stickers are 
+    commonly used for branding,<br> advertising, or providing information, 
+     and can be an important tool for businesses and organizations.
+</p>
+</div>
+<h3 class="abt">Services</h3>
+<div class="row">
+    <img src="images/IMAGE1.jpg">
+    <img src="images/IMAGE4.jpg">
+    <img src="images/IMAGE2.jpg">
+</div>
+<div class="pole1">
+    <p>VEHICLE DECALS:
+        Falcon does car decals, decorating <br>
+        vehicles using stickers, to advertise business <br>and branding.<br>
+        STICKER BRANDING:
+        A sticker marketing strategy <br> involves
+        the use of custom stickers to promotes <br>
+        your brand or business.</p>
+</div>
+<div class="pole2">
+    <P>GRAPHIC DESIGN is a craft where professionals<br>
+     create visual content to communicate messages.<br>
+     By applying visual hierarchy and page layout <br>
+     techniques, designers use typography and<br>
+     pictures to meet users' specific needs and<br>
+    focus on the logic of displaying elements in <br>
+    interactive designs to optimize the user experience</P>
+</div>
+<div class="pole3">
+    <p>ENVELOPS AND LETTERHEADS: and<br>
+        SCHOOL & WORK ID’S, CALENDARS
+        & T-SHIRT <br>SCREEN PRINTING: 
+        Falcon brands all types of <br> cloths, designs and brand ID’s and designs,<br>
+        brands and prints calendars.</p>
+</div>
+<div>
+    <p></p>
+    <br><br><br>
+</div>
+   
+
+<h3 class="abt">Clients</h3>
+<section>
+<div class="row1">
+    <img src="clients/cl.png">
+    <img src="clients/cl1.png">
+    <img src="clients/cl2.png">
+    <img src="clients/cl3.png">
+    <img src="clients/cl4.png">
+    <img src="clients/cl5.png">
+    <img src="clients/cl7.png">
+    <img src="clients/cl8.png">
+</div>
+</section>
+<br><br>
+<h3 class="abt">FAQs</h3>
+<div>
+        <h2 class="abt">How can I contact the company?<button><a href="contacts.php">+</a></button></h2><br>
+        <h2 class="abt">Are your privancy policies upto date?<button type="click"><a href="policies.php">+</a></button></h2><br>
+        <h2 class="abt">What are the Term & Condtions of using your services?<button type="click"><a href="terms.php">+</a></button></h2><br>
+        <h2 class="abt">What to know more about the services of Falcon Company?<button type="click"><a href="services.php">+</a></button></h2><br>
+        <h2 class="abt">Which company created this website?<button type="click"><a href="http://www.euphoria-hybrotect.com">+</a></button></h2>
+</div>
+ <br>
+ <div>
+    <h3 class="abt">Leave a comment</h3>
+</div>
+<div> 
+    <form class="greg" action="include/fh.inc.php" Method="POST"><br>
+    <label for="name">Names</label><br>
+    <input type="text" name="names" placeholder="names" required><br> 
+    <label for="email">Email</label><br>
+    <input type="text" name="email" placeholder="email" required><br>
+    <label for="comments">Comments</label><br>
+    <input type1="text" name="comments" id="comments" placeholder="comments" required><br>
+    <div class="btn"><button class="forl" type="submit">Submit</button></div>
+   </form>
+</div>
+<br><br><br>
+<h3 class="abt">Remarks</h3>
+ <div class="try1">
+<div class="raw">
+ <img src="images/ceo.jpg">
+</div>
+        <h4 class="try">About the C.E.O</h4>
+        <p>Mr. Stephen Odhiambo is the C.E.O of Falcon. He is a <br>
+            young vibrant man. He is a skilled businessman who <br>
+            has a diploma in Information Technology from the <br> 
+            Institute of Sigalagala. He is knowledgable in branding,<br>
+            graphic designs, computer technology and more. <br>
+            
+</div>
+<br>
+ <div class="pole">
+     <div class="raw">
+        <img src="images/fl.jpg">
+        </div> 
+        <h4 class="try">The C.E.O's Remarks</h4>
+        <p>"Do not allow challenges and failures hinder your sucess, <br>
+        let every obstacle be a stepping ground to your next victory.<br> 
+            Nothing comes without sacrifice and determination, <br> 
+            and a little hope that dispite everything, things <br>
+            or statues will eventually change. Keep on dreaming." <br>   
+        </p> 
+</div>
+        <br>
+<h3 class="abt">Business Quote</h3>
+<div class="abt">
+    <h2>"THE KEY TO REALIZING
+        A DREAM IS TO FOCUS <br>
+        NOT ON SUCCESS BUT ON
+        SIGNIFICANCE." <br> <br>
+        OPRAH WINFREY</h2>
+</div>
+
+<div class="abt">
+    <h3 class="abt">Videos</h3>
+</div>
+<div>
+    <video id="myVideo" class="video-container" controls>
+        <source src="videos/vid2.mp4" type="video/mp4">
+        <p>Your browser does not support the video tag!!</p>
+    </video>
+</div>
+<div>
+    <video id="myVideo" class="video-container" controls> 
+        <source src="videos/vid.mp4" type="video/mp4">
+        <button type="click">play/pause</button>
+        <p>Your browser does not support the video tag!!</p>
+    </video>
+</div>
+<br><br><br>
+    <sector>
+   <div> 
+        <p style="color: grey" class="abt">credits</p>
+   </div>  
+   <div>
+    <p class="abt">Images and videos used in this website were downloaded from FREEPIK. Apart from client's logos, remarks images and branding samples. <br>
+        <a href="www.freepik.com">freepik</a></P>
+   </div>
+   <div>
+    <p class="abt">This website was created by Euphoria Hybrotech. It is muilt-service business that deals with graphic designing, website <br>
+             development, Business solutions, Slogan creation, catalogue making, portfolio making and cv writing, business plans and proposals. <br>
+         <a href="www.euphoria.com">euphoria</a></P>  
+</div>  
+<div>
+    <p style="color: grey" class="abt">links</P>
+    <p class="abt">This website contains third party links, they are for educational, business and advertisement purposes only.<br>
+</div>   
+    <br><br>    
+ <div>
+    <h3 style="font-style: italic;" class="abt">Thank you for visiting our website, welcome back soon...</h3>
+   </div> 
+   <br>
+    <?php
+    require_once "footer.html";
+    ?>
+   </sector> 
+  
+
+
+
+
+
+   
+  
