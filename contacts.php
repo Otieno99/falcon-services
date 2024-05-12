e4810ac10357b32aa7e02c40fd621e8423bfec47
website <otienoi248@gmail.com>
2024-05-13 00:35:49 +0300

    Add files via upload

---
+++ contacts.php
@@ -0,0 +1,41 @@
+<?php
+  require_once "header.html";
+  ?>
+
+
+<div>
+    <h2>Contacts Information</h2>
+</div>
+<div><h4 class="abt">Want to do business with us? Contact us today <br>and get our latest offers!</h4></div>
+<div class="column">
+    <img src="images/IMAGE6.jpg">
+</div>
+<div class="tire">
+<div class="abt">
+    <h3 class="abt">Company Contact Information</h3>
+    <p>NAME: FALCON COMPUTEC</p>
+    <p>TELEPHONE NUMBER: +254735258252</p>
+    <P>EMAIL: FALCONCOMPUTEC@GMAIL.COM</P>
+<br><br>
+<hr>
+<br>
+    <h3 class="abt">C.E.O Contact Information</h3>
+    <P>NAME: STEPHEN ODHIAMBO</P>
+    <p>PHONE NUMBER: +254705727945</p>
+    <P>EMAIL: STEPHENODHIAMBO@GMAIL.COM</P>
+ <br><br>
+ <hr>
+ <br>
+ <h3 class="abt">Company Location</h3>
+    <P>COUNTRY: KENYA</P>
+    <p>COUNTY: KAKAMEGA</p>
+</div>
+</div>
+
+<br>
+<br>
+<br>
+
+   <?php
+     require_once "footer.html";
+     ?>
